f76f04abe9d69e01df97b454db3451a8aee3e944
Giheon Do <dgh0001@naver.com>
2023-11-26 12:22:47 +0900

    all link request changed to be with getsecret

--- links/urls.py
+++ links/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("all", views.AllLinks.as_view()),
+    path("all/<str:getsecret>", views.AllLinks.as_view()),
     path("addlink", views.AddLink.as_view()),
     path("<str:username>", views.OneLink.as_view()),
 ]
