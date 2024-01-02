fb5766edb9e03e9a4875e0de69bd9b3fe474e69e
Giheon Do <dgh0001@naver.com>
2024-01-02 21:59:33 +0900

    sendToday code added

--- users/views.py
+++ users/views.py
@@ -1,12 +1,19 @@
+from typing import Any
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from rest_framework import status
-from rest_framework.exceptions import ParseError
+from rest_framework.exceptions import ParseError, NotFound
 from django.contrib.auth import authenticate, login, logout
 from .models import User
+from links.models import Link
+from infos.models import Info
 from .serializers import TinyUserSerializer
+from links.serializer import LinkSerializer
+from infos.serializer import TinyInfoSerializer
 import environ, json
+import datetime as dt
+from discord_webhook import DiscordWebhook
 
 env = environ.Env(DEBUG=(bool, False))
 environ.Env.read_env()
@@ -29,9 +36,9 @@ class SignUp(APIView):
             serializer = TinyUserSerializer(new_user)
 
             user = authenticate(
-            request,
-            username=username,
-            password=password,
+                request,
+                username=username,
+                password=password,
             )
             login(request, user)
 
@@ -106,3 +113,43 @@ class DeleteErrorUsers(APIView):
             user = User.objects.get(username=link.get("owner")["username"])
             user.delete()
         return Response(status=status.HTTP_200_OK)
+
+
+class SendToday(APIView):
+    def __init__(self):
+        d = dt.datetime.now()
+        self.today = f"{d.year}-{str(d.month).zfill(2)}-{str(d.day).zfill(2)}"
+
+    def get_link(self, user):
+        try:
+            link = Link.objects.get(owner=user)
+            return link
+        except:
+            raise NotFound
+
+    def get_infos(self):
+        try:
+            infos = Info.objects.filter(date=self.today)
+            return infos
+        except:
+            raise NotFound
+
+    def get(self, request):
+        try:
+            link = self.get_link(request.user)
+            infos = self.get_infos()
+
+            linkdata = LinkSerializer(link)
+            mylink = linkdata.data.get("link")
+
+            infodata = TinyInfoSerializer(infos, many=True)
+
+            # temp_msg = ""
+            # temp_msg += "=" * 10
+            # for d in infodata:
+            #     temp_msg += f"\n{k} | {d[0]}\n{d[1]}\n{d[2]}\n"
+            # temp_msg += "=" * 10
+
+            return Response(status=status.HTTP_200_OK)
+        except:
+            return Response(status=status.HTTP_404_NOT_FOUND)
