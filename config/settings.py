b9750f8cb904708103b96c6d9083150049f43efa
Giheon Do <dgh0001@naver.com>
2024-02-02 08:12:03 +0900

    CORS_ALLOWED_ORIGINS

--- config/settings.py
+++ config/settings.py
@@ -164,13 +164,17 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 AUTH_USER_MODEL = "users.User"
 
-# CORS
-CORS_ALLOWED_ORIGINS = [
-    "http://127.0.0.1:3000",
-]
-CSRF_TRUSTED_ORIGINS = [
-    "http://127.0.0.1:3000",
-]
+if DEBUG:
+    # CORS
+    CORS_ALLOWED_ORIGINS = [
+        "http://127.0.0.1:3000",
+    ]
+    CSRF_TRUSTED_ORIGINS = [
+        "http://127.0.0.1:3000",
+    ]
+else:
+    CORS_ALLOWED_ORIGINS = ["https://knubot-frontend.onrender.com"]
+    CSRF_TRUSTED_ORIGINS = ["https://knubot-frontend.onrender.com"]
 CORS_ALLOW_CREDENTIALS = True
 
 if not DEBUG:
